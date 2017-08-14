0c6334648d573573a36129bd4724aac3d4ca8a08
zhangwenjun <zwj987625>
2017-08-14 16:22:31 +0800

    代码提交

---
+++ readMe.txt
@@ -0,0 +1,11 @@
+﻿1.在mongodb安装目录下创建data文件夹
+E:\MongoDB\Server\3.4> md  E:\MongoDB\Server\3.4\data
+2.启动mongodb:
+mongod.exe  --dbpath "E:\MongoDB\Server\3.4\data"
+3.连接客户端：
+E:\MongoDB\Server\3.4\bin> mongo.exe
+4.创建数据库：
+use springboot
+
+
+参考：http://www.tuicool.com/articles/IVzuqaj
---
+++ src/main/java/dev/local/secruity/JwtUser.java
@@ -0,0 +1,77 @@
+package dev.local.secruity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+
+public class JwtUser implements UserDetails {
+    private final String id;
+    private final String username;
+    private final String password;
+    private final String email;
+    private final Collection<? extends GrantedAuthority> authorities;
+    private final Date lastPasswordResetDate;
+
+    public JwtUser(
+            String id,
+            String username,
+            String password,
+            String email,
+            Collection<? extends GrantedAuthority> authorities,
+            Date lastPasswordResetDate) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.authorities = authorities;
+        this.lastPasswordResetDate = lastPasswordResetDate;
+    }
+
+   
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @JsonIgnore
+    public String getId() {
+        return id;
+    }
+
+    @JsonIgnore
+    public String getPassword() {
+        return password;
+    }
+
+  
+    public String getUsername() {
+        return username;
+    }
+
+    @JsonIgnore
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @JsonIgnore
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @JsonIgnore
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @JsonIgnore
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @JsonIgnore
+    public Date getLastPasswordResetDate() {
+        return lastPasswordResetDate;
+    }
+}
